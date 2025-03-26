a2aa11ce09c538418e36089ac07a45c6040c4164
Logan Sherwin <lsherwin10@gmail.com>
2025-03-25 22:43:06 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: '/',
   plugins: [react()],
-  base: mode === 'production' ? '/logansherwin/' : '/',
-}));
-
+});
