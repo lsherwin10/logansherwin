a9cffca2a01090d808a9cac9167786ad0c81c43f
Logan Sherwin <lsherwin10@gmail.com>
2025-03-25 22:17:24 -0700

    Update vite.config.js to support dev and prod bases

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/logansherwin',
-});
+  base: mode === 'production' ? '/logansherwin/' : '/',
+}));
+
